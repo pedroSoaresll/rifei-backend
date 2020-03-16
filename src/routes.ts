ecfc3f13a353e2e39ab287ac62ad487dc976f9d4
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2020-03-16 11:46:44 -0300

    remove log environmnt variable

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,11 @@
 import { Router } from 'express'
 import UsersController from './app/controllers/UsersController'
 import NewsCrawler from './app/crawlers/anvisa/NewsCrawler'
-import { sendEmail } from './libs/mailgun'
-import { render } from './libs/nunjucks'
-import { TemplatesName, getTemplate } from './helper/email'
 
 const routes = Router()
 
 routes.get('/', async (req, res) => {
-  res.json({
-    ...process.env
-  })
+  res.json({ message: 'Hello World' })
 })
 routes.get('/users', UsersController.index)
 
