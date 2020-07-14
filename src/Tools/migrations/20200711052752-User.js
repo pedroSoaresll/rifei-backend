6e053ef831d9b30816a1fcb6268b727a5565bf01
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2020-07-14 18:38:17 -0300

    update migration

--- src/Tools/migrations/20200711052752-User.js
+++ src/Tools/migrations/20200711052752-User.js
@@ -12,6 +12,7 @@ module.exports = {
     await queryInterface.createTable('users', {
       id: {
         type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
         primaryKey: true
       },
       name: {
